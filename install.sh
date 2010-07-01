7c77b87d13b4360752db579222f3dff14255dc92
Xavier Guérin <xavier.guerin@imag.fr>
2010-07-01 17:25:41 +0200

    Adapted architecture specific information to the new platforms.

--- install.sh
+++ install.sh
@@ -4,9 +4,11 @@ export APES_CC_FLAGS="-Wall -Werror -Wno-format -std=c99"
 export MJPEG_CC_FLAGS="-DNB_DECODER=2 -DDISPATCH_TIME -DINFO -DVERBOSE"
 
 APES_CC_OPTIMIZATIONS="-g -mlittle-endian -O3 -mfpu=fpa -march=armv6"
-APES_CC_OPTIMIZATIONS+=" -DDNA_ENABLE_LOG=INFO_LEVEL"
 export APES_CC_OPTIMIZATIONS
 
 export APES_COMPILER="arm-sls-dnaos-gcc"
 export APES_LINKER="arm-sls-dnaos-gcc"
 export APES_LINKER_FLAGS="-mfpu=fpa -mlittle-endian -march=armv6 -T$PWD/ldscript"
+
+# export DNACORE_CC_FLAGS="-DDNA_ENABLE_LOG=VERBOSE_LEVEL"
+unset DNACORE_CC_FLAGS
