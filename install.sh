4878a81d4ab621ab0d986a2cfe198ea9971d6ac6
hshen <hao.shen@imag.fr>
2010-09-16 17:23:20 +0200

    updated for kvm

--- install.sh
+++ install.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 export TOOLCHAINS="/home/shenhao/tmp/test_native/opt"
-export CRT0="${TOOLCHAINS}/i386-sls-dnaos/lib/crt0.o"
+export CRT0="/home/shenhao/workspace/vm/kvm/userspace/kvm-compare/kvm-85/user/test/x86/dnastart.o"
 
 export APES_COMPILER="i386-sls-dnaos-gcc"
 export APES_LINKER="i386-sls-dnaos-gcc"
 
-export APES_CC_FLAGS="-Wall -Werror -Wno-format -std=c99"
-export APES_LINKER_FLAGS="-Wl,-shared ${CRT0} -L${TOOLCHAINS}/i386-sls-dnaos/lib"
+export APES_CC_FLAGS="-D__i386__ -D__KERNEL__ -Wall -Werror -Wno-format -std=c99 -I/home/shenhao/workspace/apes.git/apps/kvmValidationFAT/Sources"
+export APES_LINKER_FLAGS="${CRT0} -D__i386__ -D__KERNEL__ -Wl,-T,elf.lds -L${TOOLCHAINS}/i386-sls-dnaos/lib"
 
 APES_CC_OPTIMIZATIONS="-g"
-#APES_CC_OPTIMIZATIONS+=" -DDNA_ENABLE_LOG=INFO_LEVEL"
 export APES_CC_OPTIMIZATIONS
 
+export DNAVIRTUALFILESYSTEM_CC_FLAGS="-DDNA_ENABLE_LOG=VERBOSE_LEVEL"
 export MJPEG_CC_FLAGS="-DNB_DECODER=1 -DDISPATCH_TIME -DINFO -DSHOW_TIME"
